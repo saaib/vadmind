a7f1fbcfece5e43939cad40d10e79730302e90f9
Urivan Flores-Saaib <urivan@saaib.net>
2016-03-01 15:23:32 -0800

    Moved into a new folder.

---
+++ lib/VAdmind/Plugins/Test/Test.pm
@@ -0,0 +1,23 @@
+package VAdmind::Plugins::Test::Test;
+
+sub new {
+    my $type = shift;
+    my $self = {@_};
+    return bless ($self, $type);
+}
+
+sub sub1 {
+    my $self = shift;
+    my $in   = $self->{'in'};
+    my $out  = $self->{'out'};
+
+    $out->{'xml'}->{'string'}->[0] = "The following elements were transfered:\n";
+
+    foreach my $key ( keys %{$in} ) {
+        #print "xml{$key}=$in->{$key}\n";
+        $out->{'xml'}->{'string'}->[0] .= "$key: $in->{$key}\n";
+    }
+    return 1;
+}
+
+1;
