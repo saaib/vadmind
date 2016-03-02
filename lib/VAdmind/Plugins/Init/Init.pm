27dc32a74de531f29041b523776bdb4e10171135
Urivan Flores-Saaib <urivan@saaib.net>
2016-03-02 01:44:55 -0800

    Updated header documentation on plugin.

--- lib/VAdmind/Plugins/Init/Init.pm
+++ lib/VAdmind/Plugins/Init/Init.pm
@@ -1,3 +1,4 @@
+
 =head1 NAME
 
 VAdmind::Plugins::Init::Init.pm - Provides functions for linux bootup configuration.
@@ -8,12 +9,28 @@ The code is being included by the VAdmind platform.
 
 my $plugin = VAdmind::Plugins::Init::Init->new;
 
+=head1 DESCRIPTION
+
+Provides methods to configure the initialization process of the system.
+
+=head1 USES
+
+=head1 AUTHOR
+
+Urivan Flores Saaib <urivan (at) saaib.net>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2003-2016 Urivan Flores Saaib <urivan (at) saaib.net>
+All rights reserved. 
+This module is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
 =cut
 
 package VAdmind::Plugins::Init::Init;
 use strict;
 use warnings;
-
 
 =head1 METHODS
 
@@ -26,9 +43,9 @@ Creates a new Init plugin object.
 =cut
 
 sub new {
-    my $type = shift;
-    my $self = {@_};
-    return bless ($self, $type);
+	my $type = shift;
+	my $self = {@_};
+	return bless( $self, $type );
 }
 
 1;
