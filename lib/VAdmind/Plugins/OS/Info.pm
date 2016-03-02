335eba35722492ee148c09fa0f0349034ee6442f
Urivan Flores-Saaib <urivan@saaib.net>
2016-03-01 17:52:55 -0800

    Updated getCpuInfo() and XML data.

--- lib/VAdmind/Plugins/OS/Info.pm
+++ lib/VAdmind/Plugins/OS/Info.pm
@@ -1,6 +1,19 @@
-package Info;
+=head1 NAME
+
+VAdmind::Plugins::OS::Info.pm - Provides information about the OS.
+
+=head1 SYNOPSIS
+
+The code is being included by the VAdmind platform.
+
+my $plugin = VAdmind::Plugins::OS::Info->new;
+
+=cut
+
+package VAdmind::Plugins::OS::Info;
 use strict;
 use warnings;
+use Data::Dumper;
 
 
 =head1 CONSTRUCTORS
@@ -15,18 +28,18 @@ sub new {
     my $type = shift;
     my $self = {@_};
     $self->{'VERSION'}  = "0.1.1";
-    $self->{'AUTHOR'}   = "Urivan Alyasid Flores Saaib <saaib_at_ciberlinux.net>";
+    $self->{'AUTHOR'}   = "Urivan Flores Saaib <urivan (at) saaib.net>";
     return bless ($self, $type);
 }
 
 
-=head2 os_ver
+=head2 getOSVer
 
 Gets OS version.
 
 =cut
 
-sub os_ver {
+sub getOSVer {
 	my $self = shift;
 	my $out = $self->{'out'};
 	my $file = '/etc/redhat-release';
@@ -43,13 +56,13 @@ sub os_ver {
 }
 
 
-=head2 kernel_ver
+=head2 getKernelVer
 
 Gets Kernel version
 
 =cut
 
-sub kernel_ver {
+sub getKernelVer {
 	my $self = shift;
 	my $out = $self->{'out'};
 	my $file = '/proc/version';
@@ -68,46 +81,79 @@ sub kernel_ver {
 }
 
 
-=head2 cpu_info
+=head2 getCpuInfo
 
-Gets the current hardware CPU information.
+Retrieves the following information regarding CPU:
+-Vendor Id.
+-CPU Family.
+-Model Name.
+-CPU MHz.
+-Cache size.
+-Siblings.
+-Flags.
+
+$plugin->getCpuInfo;
 
 =cut
 
-sub cpu_info {
-	my $self = shift;
-	my $out = $self->{'out'};
-	my $file = '/proc/cpuinfo';
-	my %data = (
-		'vendor_id' => 1,
-		'model name' => 1,
-		'cpu MHz'=> 1,
-		'bogomips' => 1
-	);
+sub getCpuInfo {
+   my $self   = shift;
+   my $out   = $self->{'out'};
+   my $file   = '/proc/cpuinfo';
+   my $fh_err = 0;
+   my $fh;
+   my %data = (
+      'vendor_id' => 1,
+      'model name' => 1,
+      'cpu MHz'=> 1,
+      'cpu family' => 1,
+      'cache size' => 1,
+      'cpu cores' => 1,
+      'core id' => 1,
+      'flags' => 1,
+      'bogomips' => 1
+   );
 
-	my $fo = open (FH, $file);
-	if (!$fo) {
-		$out->{'error'} = 'Can\'t open file '.$file.': '.$!;
-	}
-	else {
-		while (<FH>) {
-			chomp;
-			if (! $_ =~ /^$/) {
-				$_ =~ s/\t//g;
-				$_ =~ s/  */ /g;
-				my ($name, $value) = split (/:/, $_);
-				if (defined $data{$name}) {
-					$name =~ s/ /_/g;
-					$out->{'xml'}->{$name}->[0] = $value;
-				}
-			}
-		}
-		close (FH);
-	}
+   if (-f $file) {
+      open ($fh, $file) or $fh_err = 1;
+      my $proc_id = -1;
+
+      if (defined($fh)) {
+         while (<$fh>) {
+            if (/^processor/) {
+               if ( ! defined $out->{xml}->{cpuinfo} ) {
+                  $out->{xml}->{cpuinfo} = [];
+               }
+               $proc_id++;
+               push( @{$out->{xml}->{cpuinfo}}, {} );
+            }
+            if ( $proc_id > -1 ) {
+               $_ =~ s/\t//g;
+               $_ =~ s/  */ /g;
+               my ($name, $value) = split (/:/, $_);
+               if (defined $data{$name}) {
+                  $name =~ s/ /_/g;
+                  $out->{'xml'}->{cpuinfo}->[$proc_id]->{$name}->[0] = $value;
+               }
+            }
+         }
+         close($fh);
+      }
+   }
+   else {
+      $out->{'error'} = '/proc/cpuinfo not found'
+   }
 }
 
 
-sub system_load {
+
+=head2 getSystemLoad
+
+Gets the current system load.
+
+=cut
+
+sub getSystemLoad {
 	my $self = shift;
 	my $out = $self->{'out'};
 	my $file = '/proc/loadavg';
@@ -136,15 +182,15 @@ VAdmind::Plugins::OS::Info.pm - Provide OS system information.
 
 =head1 SYNOPSIS
 
-my $plugin = Info->new;
+my $plugin = VAdmind::Plugins::OS::Info->new;
 
 =head1 AUTHOR
 
-Urivan Flores Saaib <saaib_at_ciberlinux_dot_net>
+Urivan Flores Saaib <urivan (at) saaib.net>
 
 =head1 COPYRIGHT
 
-(c) 2006 Urivan Alyasid Flores Saaib <saaib_at_ciberlinux_dot_net>
+(c) 2006 Urivan Flores Saaib <urivan (at) saaib.net>
 
 =cut
 
