7148da99091476da7cd644a567c61429eaa5687d
Urivan Flores-Saaib <urivan@saaib.net>
2016-03-02 01:24:56 -0800

    Reverted to initial commit and re-wrote changes except the global vars.

--- lib/VAdmind/Plugins/Discover/Discover.pm
+++ lib/VAdmind/Plugins/Discover/Discover.pm
@@ -9,12 +9,40 @@ The code is being included by the VAdmind platform.
 
 my $plugin = VAdmind::Plugins::Discover::Discover->new;
 
+=head1 DESCRIPTION
+
+Based on the configured path->plugins from vadmind.xml, this plugin will
+traverse recursively looking for Perl module files (.pm), then it will
+attempt to read the XML description of the module and provide the data
+it requires and provides.
+
+=head1 USES
+
+File::Find
+
+=head1 AUTHOR
+
+Urivan Flores Saaib <urivan (at) saaib.net>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2003-2016 Urivan Flores Saaib <urivan (at) saaib.net>
+All rights reserved. 
+This module is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
 =cut
 
 package VAdmind::Plugins::Discover::Discover;
 use strict;
 use warnings;
 use File::Find;
+
+my $plugin_path;
+my $out;
+my $plugins = -1;
+
+=head1 METHODS
 
 =head1 CONSTRUCTORS
 
@@ -28,9 +56,8 @@ sub new {
 	my $type = shift;
 	my $self = {@_};
 
-	$self->{'VERSION'}   = "0.1";
-	$self->{'AUTHOR'}    = "Urivan Flores Saaib <urivan (at) saaib.net>";
-	$self->{plugins}     = -1;
+	$self->{'VERSION'} = '1.0';
+	$self->{'AUTHOR'}  = 'Urivan Flores Saaib <urivan (at) saaib.net>';
 	$self = bless( $self, $type );
 	$self->init();
 	return $self;
@@ -46,6 +73,8 @@ sub init {
 	my $self = shift;
 }
 
+=head1 OTHER METHODS 
+
 =head2 getPlugins
 
 Locates all plugins with XML associated data.
@@ -53,17 +82,20 @@ Locates all plugins with XML associated data.
 =cut
 
 sub getPlugins {
-	my $self   = shift;
-	$self->{plugins} = 0;
+	my $self     = shift;
+	my $config   = $self->{'config'};
+	$out         = $self->{'out'};
+	$plugin_path = $config->{'path_plugins'};
+	$plugins     = 0;
 
-	find( \&findFile($self), $self->{config}->{path_plugins} );
+	find( \&findFile, $plugin_path );
 }
 
 sub findFile {
-	my $self           = shift;
 	my $file_full_path = $File::Find::name;
 	my $retflag        = 1;
 
+	# Verify if the XML file associated to the plugin exists.
 	if ( $file_full_path =~ /\.pm$/ ) {
 		$file_full_path =~ s/\.pm$/\.xml/;
 		if ( -f $file_full_path ) {
@@ -72,13 +104,12 @@ sub findFile {
 	}
 
 	return unless $retflag == 0;
-	#return unless -f $file_full_path && $file_full_path =~ /\.[xX][mM][lL]$/;
 
-	$self->{plugins}++;
-	my $plugins = $self->{plugins};
-	my $out = $self->{out};
+	$plugins++;
+
+	# Get the Plugin name.
 	my $file_local_path = $file_full_path;
-	$file_local_path =~ s/$self->{config}->{path_plugins}//;
+	$file_local_path =~ s/$plugin_path//;
 	my ( $group, $plugin ) = ( split( /\//, $file_local_path ) )[ 1, -1 ];
 	if ( !defined $plugin && defined $group ) {
 		$plugin = $group;
@@ -86,6 +117,7 @@ sub findFile {
 	}
 	$plugin =~ s/\.xml//;
 
+	# Get the XML content from the Plugin description.
 	my $xs = new XML::Simple( KeepRoot => 1 );
 	my $ref = $xs->XMLin($file_full_path);
 
@@ -95,14 +127,22 @@ sub findFile {
 		'version' => $ref->{'plugin'}->{'version'}
 	};
 
-	$out->{'xml'}->{'plugin'}->[$plugins]->{'author'} = { 'name' => '', 'email' => '' };
-	$out->{'xml'}->{'plugin'}->[$plugins]->{'author'}->{'name'}  = $ref->{'plugin'}->{'author'}->{'name'};
-	$out->{'xml'}->{'plugin'}->[$plugins]->{'author'}->{'email'} = $ref->{'plugin'}->{'author'}->{'email'};
+	my $name  = '';
+	my $email = '';
+	if ( defined $ref->{'plugin'}->{'author'}->{'name'} ) {
+		$name = $ref->{'plugin'}->{'author'}->{'name'};
+	}
+	if ( defined $ref->{'plugin'}->{'author'}->{'email'} ) {
+		$email = $ref->{'plugin'}->{'author'}->{'email'};
+	}
+	$out->{'xml'}->{'plugin'}->[$plugins]->{'author'} = { 'name' => $name, 'email' => $email };
 
 	# We create a XML tree with the definitions found in each Plugin XML file.
 	foreach my $element ( keys %{ $ref->{'plugin'}->{'data'} } ) {
-		$out->{'xml'}->{'plugin'}->[$plugins]->{'data'}->[0]->{$element}->[0]->{'length'} = $ref->{'plugin'}->{'data'}->{$element}->{'length'};
-		$out->{'xml'}->{'plugin'}->[$plugins]->{'data'}->[0]->{$element}->[0]->{'type'}   = $ref->{'plugin'}->{'data'}->{$element}->{'type'};
+		$out->{'xml'}->{'plugin'}->[$plugins]->{'data'}->[0]->{$element}->[0] = {
+			'length' => $ref->{'plugin'}->{'data'}->{$element}->{'length'},
+			'type'   => $ref->{'plugin'}->{'data'}->{$element}->{'type'}
+		};
 	}
 
 	foreach my $element ( keys %{ $ref->{'plugin'}->{'task'} } ) {
