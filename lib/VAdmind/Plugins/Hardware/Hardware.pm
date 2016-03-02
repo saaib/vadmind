9641d490b9d20388b62afd6422fab532746ac19c
Urivan Flores-Saaib <urivan@saaib.net>
2016-03-01 17:53:24 -0800

    Removal of getCpuInfo(), moved to Plugins/Os/Info.pm

--- lib/VAdmind/Plugins/Hardware/Hardware.pm
+++ lib/VAdmind/Plugins/Hardware/Hardware.pm
@@ -32,49 +32,6 @@ sub new {
    return bless ($self, $type);
 }
 
-=head2 getCpuInfo
-
-Retrieves the following information regarding CPU:
--Vendor Id.
--CPU Family.
--Model Name.
--CPU MHz.
--Cache size.
--Siblings.
--Flags.
-
-$plugin->getCpuInfo;
-
-=cut
-
-sub getCpuInfo {
-   my $self   = shift;
-   my $out   = $self->{'out'};
-   my $file   = '/proc/cpuinfo';
-   my $fh_err = 0;
-
-   if (-f $file) {
-      my $fh    = open (fh, $file) or $fh_err = 1;
-      my $proc_id = -1;
-
-      if (defined($fh)) {
-         while (<fh>) {
-            if (/^processor/) {
-               $proc_id++;
-               $out->{'xml'}->{'cpuinfo'}->{'id'} = $proc_id;
-               $out->{'xml'}->{'cpuinfo'}->[$proc_id]->{'processor'}->[0] = (split (/:/,$_))[1];
-            }
-            (/^vendor_id/) && ($out->{'xml'}->{'cpuinfo'}->[$proc_id]->{'vendor_id'}->[0] = (split (/:/,$_))[1]);
-            $out->{'xml'}->{'cpuinfo'}->[$proc_id]->{'family'}->[0] = (split (/:/,$_))[1] if (/^cpu family/);
-            $out->{'xml'}->{'cpuinfo'}->[$proc_id]->{'model'}->[0] = (split (/:/,$_))[1] if (/^model/);
-         }
-      }
-   }
-   else {
-      $out->{'error'} = '/proc/cpuinfo not found'
-   }
-}
-
 =head2 getBiosVer
 
 Provides the BIOS version
