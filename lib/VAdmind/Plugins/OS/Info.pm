8213eb7c8dbccad4e4a7e30d7e13edbba3ed1abf
Urivan Flores-Saaib <urivan@saaib.net>
2016-03-02 22:08:32 -0800

    - Moved getCpuInfo() to VAdmind::Plugins::System - Moved getKernelVer() to VAdmind::Plugins::Kernel

--- lib/VAdmind/Plugins/OS/Info.pm
+++ lib/VAdmind/Plugins/OS/Info.pm
@@ -1,3 +1,4 @@
+
 =head1 NAME
 
 VAdmind::Plugins::OS::Info.pm - Provides information about the OS.
@@ -8,13 +9,28 @@ The code is being included by the VAdmind platform.
 
 my $plugin = VAdmind::Plugins::OS::Info->new;
 
+=head1 DESCRIPTION
+
+Provides methods to extract information from the OS.
+
+=head1 USES
+
+
+=head1 AUTHOR
+
+Urivan Flores Saaib <urivan (at) saaib.net>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2003-2016 Urivan Flores Saaib <urivan (at) saaib.net>
+
 =cut
 
 package VAdmind::Plugins::OS::Info;
 use strict;
 use warnings;
-use Data::Dumper;
 
+=head1 METHODS
 
 =head1 CONSTRUCTORS
 
@@ -25,61 +41,14 @@ Creates a new plugin object.
 =cut
 
 sub new {
-    my $type = shift;
-    my $self = {@_};
-    $self->{'VERSION'}  = "0.1.1";
-    $self->{'AUTHOR'}   = "Urivan Flores Saaib <urivan (at) saaib.net>";
-    return bless ($self, $type);
+	my $type = shift;
+	my $self = {@_};
+	$self->{'VERSION'} = "1.0";
+	$self->{'AUTHOR'}  = "Urivan Flores Saaib <urivan (at) saaib.net>";
+	return bless( $self, $type );
 }
 
-
-=head2 getOSVer
-
-Gets OS version.
-
-=cut
-
-sub getOSVer {
-	my $self = shift;
-	my $out = $self->{'out'};
-	my $file = '/etc/redhat-release';
-	my $fo = open (FH, $file);
-	if (!$fo) {
-		$out->{'error'} = "Error reading $file: $!";
-	}
-	else {
-		my $version = <FH>;
-		$version =~ s/[\n|\r]//g;
-		$out->{'xml'}->{'version'}->[0] = $version;
-	}
-	close (FH);
-}
-
-
-=head2 getKernelVer
-
-Gets Kernel version
-
-=cut
-
-sub getKernelVer {
-	my $self = shift;
-	my $out = $self->{'out'};
-	my $file = '/proc/version';
-
-	my $fo = open (FH, $file);
-	if (!$fo) {
-		$out->{'error'} = 'Cant open '.$file.': '.$!;
-	}
-	else {
-		my $data = <FH>;
-		my $version = (split (/ /,$data))[2];
-		$version =~ s/[\n|\r]//g;
-		$out->{'xml'}->{'version'}->[0] = $version;
-	}
-	close (FH);
-}
-
+=head1 OTHER METHODS 
 
 =head2 getCpuInfo
 
@@ -97,55 +66,53 @@ $plugin->getCpuInfo;
 =cut
 
 sub getCpuInfo {
-   my $self   = shift;
-   my $out   = $self->{'out'};
-   my $file   = '/proc/cpuinfo';
-   my $fh_err = 0;
-   my $fh;
-   my %data = (
-      'vendor_id' => 1,
-      'model name' => 1,
-      'cpu MHz'=> 1,
-      'cpu family' => 1,
-      'cache size' => 1,
-      'cpu cores' => 1,
-      'core id' => 1,
-      'flags' => 1,
-      'bogomips' => 1
-   );
+	my $self   = shift;
+	my $out    = $self->{'out'};
+	my $file   = '/proc/cpuinfo';
+	my $fh_err = 0;
+	my $fh;
+	my %data = (
+		'vendor_id'  => 1,
+		'model name' => 1,
+		'cpu MHz'    => 1,
+		'cpu family' => 1,
+		'cache size' => 1,
+		'cpu cores'  => 1,
+		'core id'    => 1,
+		'flags'      => 1,
+		'bogomips'   => 1
+	);
 
-   if (-f $file) {
-      open ($fh, $file) or $fh_err = 1;
-      my $proc_id = -1;
+	if ( -f $file ) {
+		open( $fh, $file ) or $fh_err = 1;
+		my $proc_id = -1;
 
-      if (defined($fh)) {
-         while (<$fh>) {
-            if (/^processor/) {
-               if ( ! defined $out->{xml}->{cpuinfo} ) {
-                  $out->{xml}->{cpuinfo} = [];
-               }
-               $proc_id++;
-               push( @{$out->{xml}->{cpuinfo}}, {} );
-            }
-            if ( $proc_id > -1 ) {
-               $_ =~ s/\t//g;
-               $_ =~ s/  */ /g;
-               my ($name, $value) = split (/:/, $_);
-               if (defined $data{$name}) {
-                  $name =~ s/ /_/g;
-                  $out->{'xml'}->{cpuinfo}->[$proc_id]->{$name}->[0] = $value;
-               }
-            }
-         }
-         close($fh);
-      }
-   }
-   else {
-      $out->{'error'} = '/proc/cpuinfo not found'
-   }
+		if ( defined($fh) ) {
+			while (<$fh>) {
+				if (/^processor/) {
+					if ( !defined $out->{xml}->{cpuinfo} ) {
+						$out->{xml}->{cpuinfo} = [];
+					}
+					$proc_id++;
+					push( @{ $out->{xml}->{cpuinfo} }, {} );
+				}
+				if ( $proc_id > -1 ) {
+					$_ =~ s/\t//g;
+					$_ =~ s/  */ /g;
+					my ( $name, $value ) = split( /:/, $_ );
+					if ( defined $data{$name} ) {
+						$name =~ s/ /_/g;
+						$out->{'xml'}->{cpuinfo}->[$proc_id]->{$name}->[0] = $value;
+					}
+				}
+			}
+			close($fh);
+		}
+	}
+	else {
+		$out->{'error'} = '/proc/cpuinfo not found';
+	}
 }
-
-
 
 =head2 getSystemLoad
 
@@ -155,42 +122,21 @@ Gets the current system load.
 
 sub getSystemLoad {
 	my $self = shift;
-	my $out = $self->{'out'};
+	my $out  = $self->{'out'};
 	my $file = '/proc/loadavg';
 
-	my $fo = open (FH, $file);
-	if (!$fo) {
-		$out->{'error'} = 'Can\'t open file '.$file.': '.$!;
+	my $fo = open( FH, $file );
+	if ( !$fo ) {
+		$out->{'error'} = 'Can\'t open file ' . $file . ': ' . $!;
 	}
 	else {
 		my $data = <FH>;
-		my ($m1, $m5, $m15) = split (/ /,$data);
-		$out->{'xml'}->{'m1'}->[0] = $m1;
-		$out->{'xml'}->{'m5'}->[0] = $m5;
+		my ( $m1, $m5, $m15 ) = split( / /, $data );
+		$out->{'xml'}->{'m1'}->[0]  = $m1;
+		$out->{'xml'}->{'m5'}->[0]  = $m5;
 		$out->{'xml'}->{'m15'}->[0] = $m15;
 	}
-	close (FH);
+	close(FH);
 }
 
 1;
-
-__END__
-
-=head1 NAME
-
-VAdmind::Plugins::OS::Info.pm - Provide OS system information.
-
-=head1 SYNOPSIS
-
-my $plugin = VAdmind::Plugins::OS::Info->new;
-
-=head1 AUTHOR
-
-Urivan Flores Saaib <urivan (at) saaib.net>
-
-=head1 COPYRIGHT
-
-(c) 2006 Urivan Flores Saaib <urivan (at) saaib.net>
-
-=cut
-
